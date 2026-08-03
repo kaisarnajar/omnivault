5884ac7b2f6ffe981167e91b308c3c69133f5ab6
Kaisar Ahmad Najar <kaisarnajar11114@gmail.com>
2026-08-03 15:07:39 +0530

    Add missing Composable import in MainActivity.kt

--- app/src/main/java/app/taskvault/MainActivity.kt
+++ app/src/main/java/app/taskvault/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
